530458df1701e4526206b994225407f12015fdeb
Mohammed Samir <samir@hellonimbly.com>
2022-11-29 16:56:14 +0530

    fine

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+export default function App() {
+  const [ust, setUst] = useState("");
+  const [ist, setIst] = useState("");
+  const parseISOString = (s) => {
+    var b = s.split(/\D+/);
+    return new Date(
+      Date.UTC(b[0], --b[1], b[2], b[3], b[4], b[5], b[6])
+    ).toString();
+  };
 
-function App() {
+  useEffect(() => {
+    if (ust) {
+      try {
+        let ist = parseISOString(ust);
+
+        setIst(ist);
+      } catch (err) {
+        console.log(err);
+        setIst("Please enter valid time!");
+      }
+    }
+  }, [ust]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label>Enter UTC Time : </label>
+      <input type="text" value={ust} onChange={(e) => setUst(e.target.value)} />
+      <br />
+      <h3>{ist}</h3>
     </div>
   );
 }
-
-export default App;
